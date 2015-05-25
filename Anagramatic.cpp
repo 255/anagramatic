9dac67ed88c4b40ef7c2ea17b56cbcd359f2538d
Wyatt Hepler <wyatt.hepler+git@gmail.com>
2015-05-25 10:30:03 -0700

    Command line options
    Fixed a bug that treated valid prefixes as valid words in single-word mode.

--- Anagramatic.cpp
+++ Anagramatic.cpp
@@ -10,9 +10,6 @@
 static Dictionary dict;
 
 void output(const std::string& result, const std::vector<std::string>& prefixes = {}) {
-  static unsigned long count = 0;
-  std::cout << std::setw(10) << ++count << ": ";
-
   for (const std::string& prefix : prefixes) {
     std::cout << prefix << " ";
   }
@@ -22,7 +19,7 @@ void output(const std::string& result, const std::vector<std::string>& prefixes 
 
 void findAnagrams(Dictionary::Iterator* it, CharCounter* counter) {
   if (counter->empty()) {
-    output(it->word());
+    if (it->isWord()) output(it->word());
     return;
   }
 
@@ -41,8 +38,8 @@ void findAnagrams(Dictionary::Iterator* it, CharCounter* counter) {
 }
 
 void findMultiwordAnagrams(Dictionary::Iterator* it, CharCounter* counter, std::vector<std::string>* prefixes) {
-  if (counter->empty() && it->isWord()) {
-    output(it->word(), *prefixes);
+  if (counter->empty()) {
+    if (it->isWord()) output(it->word(), *prefixes);
     return;
   }
 
@@ -72,18 +69,89 @@ void findMultiwordAnagrams(Dictionary::Iterator* it, CharCounter* counter, std::
 
 #include <fstream>
 
+void printUsage(const char* prog_name) {
+  std::cerr << "Find all anagrams of a word or phrase. The anagrams are output one per line.\n"
+            << "\n"
+            << "Usage:\n"
+            << "\t" << prog_name << " [-s] [-d DICTIONARY_FILE] PHRASE...\n"
+               "\n"
+               "\tPHRASE\tThe word or words of which to find anagrams.\n"
+               "\n"
+               "\t-d\tSpecify which dictionary file to use. Default: \"dictionary\"\n"
+               "\t-h\tDisplay this message. In case you forgot something?\n"
+               "\t-s\tOnly find single-word anagrams.\n"
+            << std::endl;
+}
+
+template<typename T>
+bool parseOptions(int argc, char* argv[], T* settings) {
+  for (int i = 1; i < argc; ++i) {
+    if (argv[i][0] == '-') {
+      for (char *c = &argv[i][1]; *c != '\0'; ++c) {
+        switch (*c) {
+        case 'd':
+          if (i < (argc-1)){
+            settings->dictionary = argv[++i];
+          }
+          else {
+            printf("No value provided for -%c option.\n", *c);
+            return false;
+          }
+          break;
+        case 'h': printUsage(argv[0]); return false;
+        case 's': settings->single_word = true; break;
+        default:
+          printf("Unknown option \"%s\".\n", argv[i]);
+          printUsage(argv[0]);
+          return false;
+        }
+      }
+    }
+    else {
+      settings->phrase += argv[i];
+    }
+  }
+
+  return true;
+}
+
 int main(int argc, char* argv[]) {
-  std::ifstream wordlist("good_dictionary");
+  using namespace std;
+
+  struct {
+    bool single_word = false;
+    std::string dictionary = "dictionary";
+    std::string phrase;
+  } settings;
+
+  if (!parseOptions(argc, argv, &settings)) {
+    return 1;
+  }
+
+  if (!Dictionary::processWord(&settings.phrase)) {
+    cerr << "Illegal character in " << settings.phrase
+         << ". Only the 26 characters of the Latin alphabet are allowed." << endl;
+    return 1;
+  }
+
+  std::ifstream wordlist(settings.dictionary);
+
+  if (!wordlist.is_open()) {
+    cerr << "Cannot find dictionary file \"" << settings.dictionary << "\"." << endl;
+    return 1;
+  }
+
   dict.readFromFile(wordlist);
 
-  for (int i = 1; i < argc; ++i) {
-    CharCounter counter(argv[i]);
-    std::cout << argv[i] << ": " << counter << std::endl;
-
-    Dictionary::Iterator it(dict.iterator());
-
-    //findAnagrams(&it, &counter);
+  CharCounter counter(settings.phrase);
+  Dictionary::Iterator it(dict.iterator());
+if (settings.single_word) {
+    findAnagrams(&it, &counter);
+  }
+  else {
     std::vector<std::string> prefixes;
     findMultiwordAnagrams(&it, &counter, &prefixes);
   }
+
+  return 0;
 }
